e4a7aa0eac876d54de391f8531d7d3ddca1e4d1b
felipe <felipebrostolinribeiro@gmail.com>
2024-07-18 13:46:55 -0300

    Fixed doesUserExist query

--- src/main/java/auth/repository/UserRepository.java
+++ src/main/java/auth/repository/UserRepository.java
@@ -2,6 +2,7 @@ package auth.repository;
 
 import auth.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Optional;
@@ -11,8 +12,11 @@ public class UserRepository implements PanacheRepository<User> {
 
     public boolean doesUserExists(String username, String email) {
 
-        final Optional<User> optionalUser = find("username = 1? OR email 2?", username, email)
-                .firstResultOptional();
+        final Optional<User> optionalUser = find(
+                "name = :username OR email = :email",
+                Parameters.with("username", username)
+                        .and("email", email)
+        ).firstResultOptional();
 
         return optionalUser.isPresent();
     }
