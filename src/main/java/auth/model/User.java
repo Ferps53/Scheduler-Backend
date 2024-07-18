6eecb562d6d79c9676ca9b5ddcde23c1eaeebd83
felipe <felipebrostolinribeiro@gmail.com>
2024-07-18 11:04:40 -0300

    Created User Model

---
+++ src/main/java/auth/model/User.java
@@ -0,0 +1,20 @@
+package auth.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User extends PanacheEntity {
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "email")
+    public String email;
+
+    @Column(name = "password")
+    public String password;
+}
