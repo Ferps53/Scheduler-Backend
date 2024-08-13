fb6acf28e41fbfdfb2ec0102267117fdbc81f1af
Felipe Brostolin Ribeiro <felipebrostolinribeiro@gmail.com>
2024-08-13 10:53:11 -0300

    Added @RegisterReflection in the dtos

--- src/main/java/com/scheduler/core/table/sync/dto/TableSyncDTO.java
+++ src/main/java/com/scheduler/core/table/sync/dto/TableSyncDTO.java
@@ -1,6 +1,9 @@
 package com.scheduler.core.table.sync.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.util.List;
 
+@RegisterForReflection
 public record TableSyncDTO(String name, long userId, List<?> rows) {
 }
